42ed0a90b8320904a37b6fb2a1f25b1073b1035b
ahmedsawy1 <sawy22417@gmail.com>
2023-11-18 17:54:07 +0200

    connect redux store to react

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './src/navigation/MainStack';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
 
 export default function App() {
   return (
+    <Provider store={store}>
       <NavigationContainer>
         <MainStack />
       </NavigationContainer>
+    </Provider>
   );
 }
