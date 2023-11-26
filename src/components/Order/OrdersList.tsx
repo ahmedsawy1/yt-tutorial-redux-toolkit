7f9b969f332606e9743872e7a7119b0874833e57
ahmedsawy1 <sawy22417@gmail.com>
2023-11-26 10:52:37 +0200

    finish order list

--- src/components/Order/OrdersList.tsx
+++ src/components/Order/OrdersList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {FlatList} from 'react-native';
 
 import NewOrders from './NewOrders';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
 
 const OrdersList = ({
   cartItems,
@@ -12,12 +14,13 @@ const OrdersList = ({
   addable,
 }) => {
   const cartItemsArray = []
+  const {items} = useSelector((state: RootState) => state.cart)
 
   return (
     <FlatList
       showsVerticalScrollIndicator={false}
       style={style}
-      data={cartItemsArray}
+      data={items}
       keyExtractor={(item) => item.id}
       renderItem={({item}) => (
         <NewOrders
@@ -25,6 +28,7 @@ const OrdersList = ({
           name={item.name}
           image={item.image}
           qty={item.qty}
+          // sum={item.qty * item.price}
           sum={item.sum}
           onRemove={() => onRemove(item)}
           onAdd={() => onAdd(item)}
