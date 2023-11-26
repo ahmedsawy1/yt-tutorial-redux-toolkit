e3e92dc282c9ccd8a16031efb75251c568e5e02f
ahmedsawy1 <sawy22417@gmail.com>
2023-11-26 10:54:08 +0200

    finish add to cart and remove from cart

--- src/screens/NewOrderScreen.tsx
+++ src/screens/NewOrderScreen.tsx
@@ -1,21 +1,33 @@
 import React, {useCallback} from 'react';
-import {View, StyleSheet, } from 'react-native';
+import {View, StyleSheet, SafeAreaView, } from 'react-native';
 
 import OrdersList from '../components/Order/OrdersList';
 import MyButton from '../components/MyButton';
 import MyText from '../components/MyText';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { addItem, clearCart, removeItem } from '../store/cartSlice';
 
 const NewOrderScreen = ({navigation}) => {
+ const dispatch =  useDispatch()
 
-const handleItemAdd = () => {}
-const handleItemRemove = () => {}
+const handleItemAdd = (item) => {
+  dispatch(addItem(item))
+}
+const handleItemRemove = (item) => {
+  dispatch(removeItem(item))
+}
+const {items} = useSelector((state: RootState) => state.cart)
+const totalSum = items.reduce((acc, item) => acc + item.sum, 0);
 
   return (
     <View style={styles.screen}>
+      <SafeAreaView>
+
       <OrdersList
-        cartItems={[]}
-        onAdd={handleItemAdd}
-        onRemove={handleItemRemove}
+        cartItems={[]} 
+        onAdd={(item) => handleItemAdd(item)}
+        onRemove={(item) => handleItemRemove(item)}
         deletable
         addable
         style={{marginBottom: 107}}
@@ -25,15 +37,17 @@ const handleItemRemove = () => {}
           Total Price:{'   '}
           <MyText style={styles.totalNumber}>
             {/* calc the total price */}
-            20 $
+            {totalSum?.toFixed(2)} $
           </MyText>
         </MyText>
         <MyButton
           title="Order Now"
           style={{width: 103}}
-          onPress={() => {}}
+          onPress={() => {dispatch(clearCart())}}
         />
       </View>
+      </SafeAreaView>
+
     </View>
   );
 };
